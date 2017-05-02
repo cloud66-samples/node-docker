e08d11764bc3feb36be7598b64de29cb66393c17
Vic van Gool <lvangool@users.noreply.github.com>
2017-05-02 09:16:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ connection.query("CREATE TABLE IF NOT EXISTS tests ( counter INT(6) UNSIGNED );"
 app.get("/",function(req,res){
   connection.query('SELECT counter from tests LIMIT 1', function(err, rows, fields) {
     if (!err) {
-      res.send('<span style="font-family: Arial; font-size: 30px;">I\'m running on <b>' + os.hostname() + '</b> and so far I\'v had <b>' + rows[0].counter + '</b> hits</span>');
+      res.send('<span style="font-family: Arial; font-size: 30px;">I\'m running on <b>NON MASTER BRANCH</b> and <b>' + os.hostname() + '</b> and so far I\'v had <b>' + rows[0].counter + '</b> hits</span>');
       connection.query('UPDATE tests SET counter = counter + 1;');
     } else {
       console.log('Error while performing Query.');
